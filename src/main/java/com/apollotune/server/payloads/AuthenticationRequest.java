31ba7ba974a6dea8429e1cd61046f17bb229dd52
YusufDagdeviren <ddyusufdd@gmail.com>
2024-02-21 23:33:07 +0300

    Authentication Request and Response payloads added

---
+++ src/main/java/com/apollotune/server/payloads/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.apollotune.server.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+    private String email;
+    private String password;
+}
