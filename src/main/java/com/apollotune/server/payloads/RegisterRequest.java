beed4f639b1d09c833976e333edb85f2fa07e43b
YusufDagdeviren <ddyusufdd@gmail.com>
2024-02-22 00:00:27 +0300

    RegisterRequest payload added

---
+++ src/main/java/com/apollotune/server/payloads/RegisterRequest.java
@@ -0,0 +1,19 @@
+package com.apollotune.server.payloads;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+}
