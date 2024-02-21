305e870bfd59ee99969d059b64b906af6986e093
YusufDagdeviren <ddyusufdd@gmail.com>
2024-02-21 23:10:07 +0300

    User repositories and security config added

---
+++ src/main/java/com/apollotune/server/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.apollotune.server.repositories;
+
+import com.apollotune.server.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
