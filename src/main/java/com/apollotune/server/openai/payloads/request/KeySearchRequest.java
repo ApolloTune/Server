7ec23c1fcc97df025456a41cf2273c1797502a72
YusufDagdeviren <ddyusufdd@gmail.com>
2024-02-29 18:00:50 +0300

    KeySearchRequest added

---
+++ src/main/java/com/apollotune/server/openai/payloads/request/KeySearchRequest.java
@@ -0,0 +1,29 @@
+package com.apollotune.server.openai.payloads.request;
+
+
+import com.apollotune.server.openai.enums.MusicEmotion;
+import com.apollotune.server.openai.enums.MusicType;
+import com.apollotune.server.openai.enums.MusicYear;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class KeySearchRequest {
+    private List<MusicEmotion> emotions;
+    private List<MusicType> musicTypes;
+    private List<MusicYear> musicYears;
+
+    public KeySearchRequest(){
+        emotions = new ArrayList<>();
+        musicTypes = new ArrayList<>();
+        musicYears = new ArrayList<>();
+    }
+
+}
