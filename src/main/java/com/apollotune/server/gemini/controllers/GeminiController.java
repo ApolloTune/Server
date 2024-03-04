25d5701b03855123f4f569f2933129d09e80d088
YusufDagdeviren <ddyusufdd@gmail.com>
2024-03-04 23:42:19 +0300

    Gemini vertex ai entegration completed.

--- src/main/java/com/apollotune/server/gemini/controllers/GeminiController.java
+++ src/main/java/com/apollotune/server/gemini/controllers/GeminiController.java
@@ -3,19 +3,20 @@ package com.apollotune.server.gemini.controllers;
 
 import com.apollotune.server.gemini.payloads.request.GeminiKeySearchRequest;
 import com.apollotune.server.gemini.prompt.GeminiPromptByKeySearch;
-import com.apollotune.server.openai.payloads.request.KeySearchRequest;
-import com.apollotune.server.openai.prompt.PromptByKeySearch;
-import dev.langchain4j.model.chat.ChatLanguageModel;
+import com.google.cloud.vertexai.VertexAI;
+import com.google.cloud.vertexai.api.GenerateContentResponse;
+import com.google.cloud.vertexai.generativeai.ContentMaker;
+import com.google.cloud.vertexai.generativeai.GenerativeModel;
 import dev.langchain4j.model.input.Prompt;
 import dev.langchain4j.model.input.structured.StructuredPromptProcessor;
-import dev.langchain4j.model.openai.OpenAiChatModel;
-import dev.langchain4j.model.vertexai.VertexAiChatModel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/gemini")
@@ -30,7 +31,7 @@ public class GeminiController {
     private String LOCATION;
 
     @GetMapping("/keysearchrequest")
-    public String keySearch(@RequestBody GeminiKeySearchRequest keySearchRequest) {
+    public String keySearch(@RequestBody GeminiKeySearchRequest keySearchRequest) throws IOException {
         GeminiPromptByKeySearch promptByApp = new GeminiPromptByKeySearch();
 
         promptByApp.setMusicemotion(keySearchRequest.getEmotions());
@@ -39,12 +40,16 @@ public class GeminiController {
         promptByApp.setMusiclanguages(keySearchRequest.getMusicLanguages());
 
         Prompt prompt = StructuredPromptProcessor.toPrompt(promptByApp);
-        ChatLanguageModel model = VertexAiChatModel.builder()
-                .project(PROJECT)
-                .location(LOCATION)
-                .modelName(MODEL)
-                .build();
-        return model.generate(prompt.text());
+
+        try (VertexAI vertexAI = new VertexAI(PROJECT, LOCATION)) {
+
+
+            GenerativeModel model = new GenerativeModel(MODEL, vertexAI);
+            GenerateContentResponse response = model.generateContent(ContentMaker.fromMultiModalData(
+                    prompt.text()
+            ));
+            return response.toString();
+        }
     }
 }
 
